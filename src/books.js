6c9fe26a5d6ab9b0f26e1857b06dc03bb477b31e
suy485mishra <mishrasuyash411@gmail.com>
2023-06-27 02:33:22 +0530

    Jeez

---
+++ src/books.js
@@ -0,0 +1,26 @@
+import img1 from './images/img1.jpg';
+import img2 from './images/img2.jpg';
+import img3 from './images/img3.jpg';
+export const books=[
+{
+ title : "Atomic Habits",
+ author : "James Clear",
+ img :
+  img1,
+  id:1,
+},
+{
+       title : "Ikigai",
+    author : "Francesc Miralles",
+    img :
+     img2,
+   id: 2,
+},
+{
+    title : "The Hidden Hindu",
+ author : "Akshat Gupta",
+ img :
+  img3,
+id: 3,
+},
+]
---
+++ src/testing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// the upper line is optional in react 18
+
+const testing = () => {
+  return (
+    <div>testing</div>
+  )
+}
+
+export default testing
